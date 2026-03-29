c57787825bd6c2af18989809c8a70a945c3d0a69
vikashhub6 <vkr122436@gmail.com>
2026-03-29 16:25:17 +0530

    fix: cors for production

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,12 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+
+
+app.use(cors({ 
+  origin: ['http://localhost:5173', 'https://apna-seth-sath.vercel.app'], 
+  credentials: true 
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
